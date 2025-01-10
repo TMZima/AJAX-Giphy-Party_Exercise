ccda139a85d706135b8ec79a94ee039ae6d0266e
Trey Zima <trey.m.zima@gmail.com>
2025-01-09 17:13:07 -0700

    Completed steps in README-hints

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 // look back at the <readme.md> file for some hints //
 // working API key //
-const giphyApiKey = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+const giphyApiKey = "Z4xt2m0XuWmMRab5Ii0TuxqSqlPDlD9E";
+
+// axios
+//   .get(
+//     `http://api.giphy.com/v1/gifs/search?q=ice&api_key=${giphyApiKey}&limit=10`
+//   )
+//   .then((result) => {
+//     console.log(result);
+//   });
+const img = document.getElementById("img");
+
+async function giphyRequest() {
+  const response = await axios.get(
+    `http://api.giphy.com/v1/gifs/trending?api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
+  );
+  console.log(response.data.data[0].images.downsized);
+}
+giphyRequest();
